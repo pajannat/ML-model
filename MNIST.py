9cbf19cac5a9e80b74ef81340c77e41d6d51e629
pajannat <ytchisuga@gmail.com>
2022-12-18 14:06:57 +0900

    modified MNIST.py 202212181406

--- MNIST.py
+++ MNIST.py
@@ -1,29 +1,23 @@
-# MNISTデータのダウンロード
+# ライブラリのインポート
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+
+# MNISTデータのダウンロード
 mnist = fetch_openml('mnist_784', as_frame=False)
 
 
 # 教師データ、テストデータを抽出する
-# train_test_splitのimport
-from sklearn.model_selection import train_test_split
-#実行するたびに結果が変わらないようにseedを固定する。
+# 実行するたびに結果が変わらないようにseedを固定する。
 seed = 0
 
-#「とりあえず」の評価なので、教師データは1000個、テストデータは300個とし、ラベルにより層化抽出する。)
-X_train, X_test, y_train, y_test = train_test_split(mnist.data, 
-                           mnist.target, 
-                           test_size=300, 
-                           train_size=1000, 
-                           random_state=seed, 
-                           stratify=mnist.target)
+# 「とりあえず」の評価なので、教師データは1000個、テストデータは300個とし、ラベルにより層化抽出する。)
+X_train, X_test, y_train, y_test = train_test_split(mnist.data, mnist.target, test_size=300, train_size=1000, random_state=seed, stratify=mnist.target)
 
-#0から255までの値なので255で割って規格化する。
-X_train = X_train /255
-X_test = X_test /255
-
-
-# ライブラリのインポート
-from sklearn.svm import SVC
+# 0から255までの値なので255で割って規格化する。
+X_train = X_train / 255
+X_test = X_test / 255
 
 
 # 分類器: サポートベクターマシン(カーネルはRBF)
@@ -31,7 +25,6 @@ classifier = SVC(gamma="auto", random_state=seed)
 
 
 # 交差検証(10分割）を実行
-from sklearn.model_selection import cross_val_score
 cv_scores = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
 
 
