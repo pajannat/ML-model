0478e3b23b92958c7824e376088ca9165642798a
pajannat <ytchisuga@gmail.com>
2022-12-18 22:57:26 +0900

    modified script.yml, test_mnist_784.py

--- test_mnist_784.py
+++ test_mnist_784.py
@@ -7,7 +7,7 @@ import mnist_784
 cv_scores = mnist_784.cv_scores
 
 # スコアを出力
-print(sum(cv_scores) / len(cv_scores))
+print(f"score: {sum(cv_scores) / len(cv_scores)}")
 
 # 精度が0.85以下の場合、例外を発生させる
 if sum(cv_scores) / len(cv_scores) <= 0.85:
